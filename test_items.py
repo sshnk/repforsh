25f371f2c7c4f51ecf90de20ab4975cb67a1cb5e
sshnk <72258731+sshnk@users.noreply.github.com>
2020-10-06 18:18:28 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 import time
 
-def test_languege(browser):
+def test_guest_should_see_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(30)
     btn = browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary")
